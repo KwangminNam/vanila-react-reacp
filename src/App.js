870a102fce8252c719d876d4dd83cdc2313250b2
kwangminNam <johnpower@naver.com>
2022-07-30 00:10:44 +0900

    tsts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Daylist from './components/Daylist';
-import Day from './components/Day';
+import Day from './components/Day.tsx';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import EmptyPage from './components/EmptyPage';
 import CreateWord from './components/CreateWord';
